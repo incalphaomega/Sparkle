0e3ac3e81d9501c29d23e1825b677f2e8d25d631
Alya-PC <Alya-PC@DESKTOP-TTSQC2H>
2018-10-05 16:22:57 +0300

    Создал Player

---
+++ Sparkle/Handlers/Render.cs
@@ -0,0 +1,108 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sparkle.Handlers
+{
+    class Render
+    {
+        public Texture2D texture { get; set; }
+        public Rectangle sourceRectangle { get; set; }
+        public Color color { get; set; }
+        /// <summary>
+        /// Угол вращения
+        /// </summary>
+        public float angle { get; set; }
+        /// <summary>
+        /// Центр вращения
+        /// </summary>
+        public Vector2 origin { get; set; }
+        /// <summary>
+        /// Размер в процентах
+        /// </summary>
+        public float size { get; set; }
+        /// <summary>
+        /// Отражение
+        /// </summary>
+        public SpriteEffects effect { get; set; }
+
+        public int alphaLevel { get; set; }
+
+        float rotation, alpha, scale;
+
+        public Render()
+        {
+            texture = new Texture2D(Main.graphics.GraphicsDevice, 1, 1);
+            sourceRectangle = fullTexture(texture);
+            origin = Vector2.Zero;
+            angle = 0;
+            color = Color.White;
+            alphaLevel = 100;
+            size = 100;
+            effect = SpriteEffects.None;
+        }
+
+        public void Draw(Vector2 position)
+        {
+            if(size >= 100)
+            {
+                size = 100;
+            }
+            else if (size <= 0)
+            {
+                size = 0;
+            }
+            if (alphaLevel >= 100)
+            {
+                alphaLevel = 100;
+            }
+            else if (alphaLevel <= 0)
+            {
+                alphaLevel = 0;
+            }
+            scale = size / 100;
+            rotation = (float)MathHelper.ToRadians(angle);
+            alpha = (float)alphaLevel / 100;
+            Main.spriteBatch.Draw(texture, position, sourceRectangle, new Color(color, alpha), rotation, origin, scale, effect, 0);
+        }
+
+        public void setTexture(Texture2D texture)
+        {
+            this.texture = texture;
+            sourceRectangle = fullTexture(texture);
+        }
+
+        /// <summary>
+        /// Возвращает Rectangle по физическим масштабам текстуры
+        /// </summary>
+        /// <param name="texture"></param>
+        /// <returns></returns>
+        public Rectangle textureRectangle(Vector2 position)
+        {
+            int width = (int)Math.Floor(texture.Width * scale);
+            int height = (int)Math.Floor(texture.Height * scale);
+            if (origin == Vector2.Zero)
+            {
+                return new Rectangle((int)position.X, (int)position.Y, width, height);
+            }
+            else
+            {
+                return new Rectangle((int)position.X - width/2, (int)position.Y - height/2, width, height);
+            }
+        }
+
+        public static Rectangle fullTexture(Texture2D texture)
+        {
+            return new Rectangle(0, 0, texture.Width, texture.Height);
+        }
+
+        public static Vector2 textureCenter(Texture2D texture)
+        {
+            return new Vector2(texture.Width / 2, texture.Height / 2);
+        }
+    }
+}
---
+++ Sparkle/Player.cs
@@ -0,0 +1,93 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Sparkle.Handlers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using static Sparkle.Handlers.Input;
+
+namespace Sparkle
+{
+    class Player
+    {
+        Texture2D texture;
+        Vector2 position;
+        Rectangle rectangle;
+        Texture2D rectangleTex;
+
+        Render render;
+
+        public Player(Vector2 position)
+        {
+            this.position = position;
+            render = new Render();
+            //render.size = 25;
+            rectangleTex = new Texture2D(Main.graphics.GraphicsDevice, 1, 1, false, SurfaceFormat.Color);
+            rectangleTex.SetData(new[] { Color.White });
+        }
+
+        public void LoadContent()
+        {
+            texture = Main.content.Load<Texture2D>("Assassin");
+            render.setTexture(texture);
+            render.origin = Render.textureCenter(texture);
+        }
+        public void Update()
+        {
+            rectangle = render.textureRectangle(position);
+            if (rectangle.Contains(mousePoint))
+            {
+                render.color = Color.Red;
+            }
+            else
+            {
+                render.color = Color.White;
+            }
+            if (isKeyDown(Keys.A))
+            {
+                position.X -= 2;
+            }
+            if (isKeyDown(Keys.D))
+            {
+                position.X += 2;
+
+            }
+            if (isKeyDown(Keys.W))
+            {
+                position.Y -= 2;
+            }
+            if (isKeyDown(Keys.S))
+            {
+                position.Y += 2;
+            }
+            if (isKeyDown(Keys.Q))
+            {
+                render.size--;
+            }
+            if (isKeyDown(Keys.E))
+            {
+                render.size++;
+            }
+            //if (mousePosition.X > position.X)
+            //{
+            //    render.effect = SpriteEffects.None;
+            //}
+            //else if (mousePosition.X < position.X)
+            //{
+            //    render.effect = SpriteEffects.FlipHorizontally;
+            //}
+        }
+
+        public void Draw()
+        {
+            render.Draw(position);
+            Main.spriteBatch.Draw(rectangleTex, rectangle, new Color(Color.Gray, 70));
+        }
+
+
+    }
+}
